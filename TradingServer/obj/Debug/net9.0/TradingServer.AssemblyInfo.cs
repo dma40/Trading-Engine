4108fd7ac73473b25c52bc782843b21eb3756402
dma40 <dma99878@gmail.com>
2025-04-09 16:34:42 -0400

    update files

--- TradingServer/obj/Debug/net9.0/TradingServer.AssemblyInfo.cs
+++ TradingServer/obj/Debug/net9.0/TradingServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingServer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edf56245c96f90c684f6fa51cadb65f1cc5cbe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8214b497fb9a29f291df051badf4b9ba413f1265")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingServer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingServer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
