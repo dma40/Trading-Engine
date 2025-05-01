68dd17113d9d586864968683718e2e4820e19a3b
dma40 <dma99878@gmail.com>
2025-05-01 16:03:10 -0400

    Update Program.cs

--- TradingServer/Program.cs
+++ TradingServer/Program.cs
@@ -1,12 +1,17 @@
 ﻿using TradingServer.Tests;
 using TradingServer.Core;
+using Microsoft.AspNetCore.Components.Forms;
 
 using var engine = TradingHostBuilder.BuildTradingServer();
 TradingServerServiceProvider.serviceProvider = engine.Services;
 
 _ = Task.Run(() => processInputs());
 
-OrderbookUnitTests.runTests();
+Console.WriteLine("Run orderbook tests? y/n");
+string input = Console.ReadLine() ?? "n";
+
+if (input == "y")
+    OrderbookUnitTests.runTests();
 
 Console.WriteLine("Starting trading server...\n");
 await engine.StartAsync().ConfigureAwait(false);
