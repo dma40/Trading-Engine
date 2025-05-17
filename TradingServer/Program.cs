2073426633f3f5641efa3ec520c2fe8c8fa1f1d0
dma40 <dma99878@gmail.com>
2025-05-16 21:57:07 -0400

    update Program.cs

--- TradingServer/Program.cs
+++ TradingServer/Program.cs
@@ -1,8 +1,6 @@
 ﻿using TradingServer.Tests;
 using TradingServer.Core;
 
-Console.WriteLine(DateTime.Now.Hour > 9.5 && DateTime.Now.Hour < 16);
-
 /*
 Console.WriteLine("Run orderbook tests? (y/n)");
 string input = Console.ReadLine() ?? "n";
