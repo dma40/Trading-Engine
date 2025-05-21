f1c7e01f9a02bb6ce5f8bc5028314358da97e9b6
dma40 <dma99878@gmail.com>
2025-05-21 15:33:18 -0400

    Update db_credentials.sh

--- db_credentials.sh
+++ db_credentials.sh
@@ -4,4 +4,4 @@ export SQL_USER="sql-username"
 export SQL_PASS="sql-password"
 
 echo "MYSQL_USER is set to: $MYSQL_USER"
-echo "MYSQL_PASS is set to: $MYSQL_PASS"
+echo "MYSQL_PASS is set to: $MYSQL_PASS"å
