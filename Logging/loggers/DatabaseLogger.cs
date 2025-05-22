0e54f7452cc572ceb277816411f8b2a9024887ec
dma40 <dma99878@gmail.com>
2025-05-21 21:04:23 -0400

    Update DatabaseLogger.cs

--- Logging/loggers/DatabaseLogger.cs
+++ Logging/loggers/DatabaseLogger.cs
@@ -82,7 +82,7 @@ namespace TradingServer.Logging
 
             catch (OperationCanceledException)
             {
-                Console.WriteLine("Logger has been shut down.");
+                Console.WriteLine("Logger has shut down.");
             }
         }
 
